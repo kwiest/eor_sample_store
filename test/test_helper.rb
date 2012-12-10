175ca19375c9f0b19810b149cae2009fc82a28ce
Kyle Wiest <kyle.wiest@gmail.com>
2012-12-10 10:16:32 -0800

    Prepare test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'mocha'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -8,6 +10,8 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
 end
