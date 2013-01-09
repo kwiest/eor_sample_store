30122fb1a9ec1f02e8e21c4d440871e73038d4ce
Kyle Wiest <kyle.wiest@gmail.com>
2013-01-08 18:34:41 -0800

    Hanlde bad model ids in params or URIs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_cart
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def current_cart
     session[:shopping_cart] ||= ShoppingCart.new
+  end
+
+  def record_not_found
+    redirect_to root_path, alert: "Sorry, but we couldn't find the page you're looking for."
   end
 end
